1cf75c6a9580b4107fec9d97493f79fe9e5c7983
TboyxfireXD <CTechDevWorks@gmail.com>
2025-07-19 12:54:51 +0200

    rewrote calculator structure

--- ctcalculator/src/components/Calculator.jsx
+++ ctcalculator/src/components/Calculator.jsx
@@ -1,90 +1,51 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Calculator.css";
 
 const Calculator = () => {
-  const [result, setResult] = useState("");
-
-  const handleClick = (e) => {
-    setResult(result.concat(e.target.name));
-  };
-
-  const clear = () => {
-    setResult("");
-  };
-
-  const backspace = () => {
-    setResult(result.slice(0, -1));
-  };
-
-  const calculate = () => {
-    try {
-      setResult(eval(result).toString());
-    } catch (err) {
-      setResult("Error");
-    }
-  };
   return (
-    <div>
-      <div className="container">
-        <form>
-          <input type="text" value={result} />
-        </form>
+    <div className="calculator">
+        <input type="text" className="display" readOnly />
+      <div className="keypad">
+        <div className="row">
+          <button className="special">sin</button>
+          <button className="special">cos</button>
+          <button className="special">tan</button>
+          <button className="special">log</button>
+        </div>
 
-        <div className="keypad">
-          <button className="highlight" onClick={clear} id="clear">
-            Clear
-          </button>
-          <button className="highlight" onClick={backspace} id="backspace">
-            C
-          </button>
-          <button className="highlight" name="/" onClick={handleClick}>
-            &divide;
-          </button>
-          <button name="7" onClick={handleClick}>
-            7
-          </button>
-          <button name="8" onClick={handleClick}>
-            8
-          </button>
-          <button name="9" onClick={handleClick}>
-            9
-          </button>
-          <button className="highlight" name="*" onClick={handleClick}>
-            &times;
-          </button>
-          <button name="4" onClick={handleClick}>
-            4
-          </button>
-          <button name="5" onClick={handleClick}>
-            5
-          </button>
-          <button name="6" onClick={handleClick}>
-            6
-          </button>
-          <button className="highlight" name="-" onClick={handleClick}>
-            &ndash;
-          </button>
-          <button name="1" onClick={handleClick}>
-            1
-          </button>
-          <button name="2" onClick={handleClick}>
-            2
-          </button>
-          <button name="3" onClick={handleClick}>
-            3
-          </button>
-          <button className="highlight" name="+" onClick={handleClick}>
-            +
-          </button>
-          <button name="0" onClick={handleClick}>
-            0
-          </button>
-          <button name="." onClick={handleClick}>
-            .
-          </button>
-          <button className="highlight" onClick={calculate} id="result">
-            =
-          </button>
+        <div className="row">
+          <button className="high">C</button>
+          <button className="high">⌫</button>
+          <button className="special">/</button>
+          <button className="special">*</button>
+        </div>
+
+        <div className="row">
+          <button className="number">7</button>
+          <button className="number">8</button>
+          <button className="number">9</button>
+          <button className="special">-</button>
+        </div>
+
+        <div className="row">
+          <button className="number">4</button>
+          <button className="number">5</button>
+          <button className="number">6</button>
+          <button className="special">+</button>
+        </div>
+
+        <div className="row">
+          <button className="number">1</button>
+          <button className="number">2</button>
+          <button className="number">3</button>
+          <button className="high">=</button>
+        </div>
+
+        <div className="row">
+          <button className="number">0</button>
+          <button className="number">.</button>
+          <button className="special">^</button>
+          <button className="special">√</button>
         </div>
       </div>
     </div>
